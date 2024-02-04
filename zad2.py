68c0ac760c7c387319e9280558a17af94baab35d
NEKETA <molnei1996@gmail.com>
2024-02-04 14:15:19 +0300

    add_zad

---
+++ zad2.py
@@ -0,0 +1,42 @@
+code="""
+import os
+
+def rename_files(desired_name, num_digits, source_ext, target_ext, name_range=None):
+    new_names = []
+
+    # Получаем список файлов в текущей директории
+    files = os.listdir('test_folder')
+
+    # Фильтруем только нужные файлы с указанным расширением
+    filtered_files = [file for file in files if file.endswith(source_ext)]
+
+    # Сортируем файлы по имени
+    filtered_files.sort()
+
+    # Задаем начальный номер для порядкового номера
+    num = 1
+
+    # Переименовываем файлы
+    for file in filtered_files:
+        # Получаем имя файла без расширения
+        name = os.path.splitext(file)[0]
+
+        # Если задан диапазон, обрезаем имя файла
+        if name_range:
+            name = name[name_range[0]-1:name_range[1]]
+
+        # Создаем новое имя с желаемым именем, порядковым номером и новым расширением
+        new_name = desired_name + str(num).zfill(num_digits) + '.' + target_ext
+
+        # Переименовываем файл
+        path_old = os.path.join(os.getcwd(), folder_name, file)
+        path_new = os.path.join(os.getcwd(), folder_name, new_name)
+        os.rename(path_old, path_new)
+
+        # Увеличиваем порядковый номер
+        num += 1
+
+rename_files(desired_name="new_file_", num_digits=3, source_ext="txt", target_ext="doc")
+"""
+with open('__init__.py', 'w', encoding='utf-8') as f:
+    f.write(code)
